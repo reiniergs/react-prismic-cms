49267790cc6ee1f2128366a1966f758a20512298
TahimiLeonBravo <tahimileon@gmail.com>
2018-10-16 23:19:54 -0500

    fix: styles in exampleCard

--- src/library/ExampleCard/index.js
+++ src/library/ExampleCard/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import ButtonIcon from 'react-rainbow-components/components/ButtonIcon';
+import Button from 'react-rainbow-components/components/Button';
+import Card from 'react-rainbow-components/components/Card';
+import ShareIcon from './shareIcon';
 import './styles.css';
 
 function CardItems({ results }) {
     return results.map(({ data, id }) => (
-        <div key={id} className="prismic-cms-card-item">
+        <Card
+            className="prismic-cms-card-item"
+            key={id}
+            actions={
+                <ButtonIcon
+                variant="border-filled" 
+                icon={<ShareIcon />} />}
+            footer={
+                <div className="prismic-cms-card-item_content">
+                    <h1 className="prismic-cms-card-item_header">{data.name[0].text}</h1>
+                    <p className="prismic-cms-card-item_description">{data.description[0].text}</p>
+                    <Button
+                        className="prismic-cms-card-item_button"
+                        variant="outline-brand"
+                        label="More" />
+                </div>
+            }>
             <div className="prismic-cms-card-item_img-container">
                 <img src={data.image.url} alt="" />
             </div>
-            <div className="prismic-cms-card-item_description-container">
-                <h1 className="prismic-cms-card-item_header">{data.name[0].text}</h1>
-                <p className="prismic-cms-card-item_description">{data.description[0].text}</p>
-            </div>
-        </div>
+        </Card>
     ));
 }
 
