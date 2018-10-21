c97b1d9b9da2a43f9e011cf14b53623cb7863515
LeandroTorresSicilia <jltorressicilia@gmail.com>
2018-10-21 18:39:39 -0400

    fix: updated export index

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,61 @@
 import Prismic from './Prismic';
+import QueryAny from './QueryAny';
 import QueryAt from './QueryAt';
+import QueryDateAfter from './QueryDateAfter';
+import QueryDateBefore from './QueryDateBefore';
+import QueryDateBetween from './QueryDateBetween';
+import QueryDayOfMonth from './QueryDayOfMonth';
+import QueryDayOfMonthAfter from './QueryDayOfMonthAfter';
+import QueryDayOfMonthBefore from './QueryDayOfMonthBefore';
+import QueryDayOfWeek from './QueryDayOfWeek';
+import QueryDayOfWeekAfter from './QueryDayOfWeekAfter';
+import QueryDayOfWeekBefore from './QueryDayOfWeekBefore';
 import QueryFullText from './QueryFullText';
+import QueryGreaterThan from './QueryGreaterThan';
+import QueryHas from './QueryHas';
+import QueryHour from './QueryHour';
+import QueryHourAfter from './QueryHourAfter';
+import QueryHourBefore from './QueryHourBefore';
+import QueryIn from './QueryIn';
+import QueryInRange from './QueryInRange';
+import QueryLessThan from './QueryLessThan';
+import QueryMissing from './QueryMissing';
+import QueryMonth from './QueryMonth';
+import QueryMonthAfter from './QueryMonthAfter';
+import QueryMonthBefore from './QueryMonthBefore';
 import QueryMulti from './QueryMulti';
 import QueryNot from './QueryNot';
+import QuerySimilar from './QuerySimilar';
+import QueryYear from './QueryYear';
 
 export {
     Prismic,
+    QueryAny,
     QueryAt,
+    QueryDateAfter,
+    QueryDateBefore,
+    QueryDateBetween,
+    QueryDayOfMonth,
+    QueryDayOfMonthAfter,
+    QueryDayOfMonthBefore,
+    QueryDayOfWeek,
+    QueryDayOfWeekAfter,
+    QueryDayOfWeekBefore,
     QueryFullText,
+    QueryGreaterThan,
+    QueryHas,
+    QueryHour,
+    QueryHourAfter,
+    QueryHourBefore,
+    QueryIn,
+    QueryInRange,
+    QueryLessThan,
+    QueryMissing,
+    QueryMonth,
+    QueryMonthAfter,
+    QueryMonthBefore,
     QueryMulti,
     QueryNot,
+    QuerySimilar,
+    QueryYear,
 };
