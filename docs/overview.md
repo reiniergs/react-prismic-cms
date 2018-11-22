f55469a7e972f5b2c982d1dccbcb0622b7dbee47
Tahimi <32083556+TahimiLeonBravo@users.noreply.github.com>
2018-11-22 01:59:31 -0600

    feat: implement overview page (#40)

---
+++ docs/overview.md
@@ -0,0 +1,18 @@
+### Take the stress out of managing a website. By nature, content management systems support the separation of content and presentation. This is what you will accomplish with this  React Library on top of Prismic.io
+
+## Advantages
+
+* Fetches the content through declarative components that allow you to focus only in the experience interface.
+* Platform agnostic (react & react native)
+* Reduces need to code from scratch
+
+## A few use cases
+
+* Listing items by an specific criteria
+* Changing & hiding content without the need of new build.
+* Filtering content with tags
+* Content localization
+
+## Interactive Examples
+
+Proper documentation based on interactive examples to help you understand the features of each component.
--- styleguide.config.js
+++ styleguide.config.js
@@ -13,12 +13,47 @@ module.exports = withRainbowStyles({
     skipComponentsWithoutExample: true,
     pagePerSection: true,
     ribbon: { url: 'https://github.com/reiniergs/react-prismic-cms' },
+    template: {
+        favicon: 'https://react-prismic-cms.firebaseapp.com/favicon.ico',
+        head: {
+            meta: [
+                {
+                    name: 'robots',
+                    content: 'index,follow'
+                },
+                {
+                    name: 'description',
+                    content: 'Set of declarative component to query content published in the headless CMS prismic.io'
+                },
+                {
+                    name: 'keywords',
+                    content: 'react, prismic, cms, content, components, library'
+                },
+                {
+                    property: 'og:title',
+                    content: 'React Prismic CMS'
+                },
+                {
+                    property: 'og:description',
+                    content: 'Set of declarative component to query content published in the headless CMS prismic.io'
+                },
+                {
+                    property: 'og:image',
+                    content: 'https://react-rainbow.firebaseapp.com/share-image.png'
+                }
+            ]
+        },
+    },
     sections: [
         {
             name: 'Getting started',
             sectionDepth: 1,
             content: 'docs/usage.md',
             sections: [
+                {
+                    name: 'Overview',
+                    content: 'docs/overview.md',
+                },
                 {
                     name: 'Usage',
                     content: 'docs/usage.md',
